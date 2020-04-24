3afff0596d594ba23d8762b0b9bde200578f91a6
KariK <kari@bighillops.com>
2020-04-24 11:04:05 +0300

    Base files added

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+// On demand installation of the app
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  console.log("Installable...");
+  //showInstallPromotion();
+});
+
+menuInstall = () => {
+  alert("asenna");
+  // Hide the app provided install promotion
+  //hideMyInstallPromotion();
+ 
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  })
+}
+
+// Track if app somehow installed
+window.addEventListener('appinstalled', (evt) => {
+  console.log('a2hs installed');
+});
+
+// Menu reset feature
+menuReset = () => {
+  alert("resetoi");
+}
+
+// Menu info feature
+menuInfo = () => {
+  alert("Versio 0.1");
+}
+
+// Load up and start SW script
+window.onload = () => {  
+    'use strict';     
+    if ('serviceWorker' in navigator) {     
+        navigator.serviceWorker.register('./sw.js'); 
+    } 
+}
+
+// Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon 
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+      
+// Get today's date 
+n =  new Date();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = "(Tänään on "+ d + "." + m + ".)";
---
+++ sw.js
@@ -0,0 +1,27 @@
+
+var cacheName = 'hello-pwa'; 
+
+var filesToCache = [
+    '/',    
+    '/index.html',    
+    '/css/style.css',  
+    '/js/main.js'  
+];  
+
+// Add all files to cache once SW installed
+self.addEventListener('install', function(e) { 
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) { 
+            return cache.addAll(filesToCache);   
+        })    
+    );  
+});
+
+/* Serve cached content when offline */ 
+self.addEventListener('fetch', function(e) {  
+    e.respondWith( 
+        caches.match(e.request).then(function(response) {  
+            return response || fetch(e.request);
+        })   
+    );  
+});
